3070aa8f4a5e31c1671795652610ad1601a9f9ae
PreetiSarojAcharya <Preeti.saroj@gmail.com>
2020-06-08 19:35:40 +0530

    commit

--- angularProject/src/app/app.component.ts
+++ angularProject/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  // book
+  selector: 'app-start',
+  template: '<h1>Hello Angular Preeti..!</h1>', // view : UI
 })
 export class AppComponent {
-  title = 'angularProject';
+  // data and logic
 }
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'],
+// })
+// export class AppComponent {
+//   title = 'angularProject';
+// }
--- angularProject/src/app/app.module.ts
+++ angularProject/src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule], // module : group of logics
+  bootstrap: [AppComponent], // to run : root compo
 })
-export class AppModule { }
+export class AppModule {}
+
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
