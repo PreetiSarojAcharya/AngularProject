afeacf876e4361ee32ccf8b88992d86224364263
PreetiSarojAcharya <Preeti.saroj@gmail.com>
2020-06-13 11:04:50 +0530

    commit

--- angularProject/src/app/layouts/products/products.component.ts
+++ angularProject/src/app/layouts/products/products.component.ts
@@ -9,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
           <div class="col s12 m7">
             <div class="card">
               <div class="card-image">
-                <img src="../../../assets/img/_PP08880.jpg" />
-                <span class="card-title">Card Title</span>
+                <img [src]="mobPart.imagepath" />
+
+                <span class="card-title"
+                  ><b>{{ mobPart.cardtitle }}</b></span
+                >
               </div>
               <div class="card-content">
                 <p>
@@ -81,28 +84,34 @@ export class ProductsComponent implements OnInit {
 
   mobParts = [
     {
+      cardtitle: 'Chef baby',
       id: 1001,
       name: 'ScreenPreeti 5.5',
       description: '5.5 Screen for Moto g',
       inStock: 5,
       prodColor: 'red',
       canPurch: true,
+      imagepath: '../../../assets/img/_PP08880.jpg',
     },
     {
+      cardtitle: 'Nature',
       id: 1002,
       name: 'Screen 5',
       description: '5 Screen for Samsung',
       inStock: 8,
       prodColor: 'green',
       canPurch: false,
+      imagepath: '../../../assets/img/sample-1.jpg',
     },
     {
+      cardtitle: 'Office',
       id: 1003,
       name: 'key panel',
       description: 'Key panel for Nokia',
       inStock: 0,
       prodColor: 'blue',
       canPurch: true,
+      imagepath: '../../../assets/img/office.jpg',
     },
   ];
 }
